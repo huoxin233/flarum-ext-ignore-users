196dfb76129cbb529894f3ddb5b09f399c239359
huoxin233 <23447157+huoxin233@users.noreply.github.com>
2026-02-21 00:56:51 +0800

    fix: enhance visual for ignore user setting

--- js/src/forum/addSettingsPage.js
+++ js/src/forum/addSettingsPage.js
@@ -1,10 +1,12 @@
-import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
+import { extend } from 'flarum/common/extend';
+
 import SettingsPage from 'flarum/forum/components/SettingsPage';
+import FieldSet from 'flarum/common/components/FieldSet';
 import Select from 'flarum/common/components/Select';
 
-export default function () {
-  extend(SettingsPage.prototype, 'privacyItems', function (items) {
+export default () => {
+  extend(SettingsPage.prototype, 'settingsItems', function (items) {
     const preferences = this.user.preferences();
 
     const behaviorOptions = {
@@ -13,49 +15,49 @@ export default function () {
     };
 
     items.add(
-      'ignored-discussion-behavior',
-      <div className="Form-group">
-        <label>{app.translator.trans('fof-ignore-users.forum.settings.ignored_discussion_default_behavior_label')}</label>
-        <Select
-          value={
-            preferences['fof-ignore-users.ignored_discussion_behavior'] ||
-            app.forum.attribute('fof-ignore-users.ignored_discussion_default_behavior') ||
-            'hide'
-          }
-          options={behaviorOptions}
-          onchange={(value) => {
-            this.ignoredDiscussionBehaviorLoading = true;
-            this.user.savePreferences({ 'fof-ignore-users.ignored_discussion_behavior': value }).then(() => {
-              this.ignoredDiscussionBehaviorLoading = false;
-              m.redraw();
-            });
-          }}
-          loading={this.ignoredDiscussionBehaviorLoading}
-        />
-        <p className="helpText">{app.translator.trans('fof-ignore-users.forum.settings.ignored_discussion_behavior_help')}</p>
-      </div>
-    );
+      'fof-ignore-users',
+      FieldSet.component(
+        {
+          label: app.translator.trans('fof-ignore-users.forum.settings.settings_heading'),
+          className: 'Settings-ignoreUsers',
+        },
+        [
+          <div className="Form-group">
+            <p>{app.translator.trans('fof-ignore-users.forum.settings.ignored_discussion_default_behavior_label')}</p>
 
-    items.add(
-      'ignored-post-behavior',
-      <div className="Form-group">
-        <label>{app.translator.trans('fof-ignore-users.forum.settings.ignored_post_default_behavior_label')}</label>
-        <Select
-          value={
-            preferences['fof-ignore-users.ignored_post_behavior'] || app.forum.attribute('fof-ignore-users.ignored_post_default_behavior') || 'hide'
-          }
-          options={behaviorOptions}
-          onchange={(value) => {
-            this.ignoredPostBehaviorLoading = true;
-            this.user.savePreferences({ 'fof-ignore-users.ignored_post_behavior': value }).then(() => {
-              this.ignoredPostBehaviorLoading = false;
-              m.redraw();
-            });
-          }}
-          loading={this.ignoredPostBehaviorLoading}
-        />
-        <p className="helpText">{app.translator.trans('fof-ignore-users.forum.settings.ignored_post_behavior_help')}</p>
-      </div>
+            {Select.component({
+              options: behaviorOptions,
+              value:
+                preferences['fof-ignore-users.ignored_discussion_behavior'] ||
+                app.forum.attribute('fof-ignore-users.ignored_discussion_default_behavior') ||
+                'hide',
+              onchange: (value) => {
+                this.user.savePreferences({ 'fof-ignore-users.ignored_discussion_behavior': value }).then(() => m.redraw());
+              },
+            })}
+
+            <p className="helpText">{app.translator.trans('fof-ignore-users.forum.settings.ignored_discussion_default_behavior_help')}</p>
+          </div>,
+
+          <div className="Form-group">
+            <p>{app.translator.trans('fof-ignore-users.forum.settings.ignored_post_default_behavior_label')}</p>
+
+            {Select.component({
+              options: behaviorOptions,
+              value:
+                preferences['fof-ignore-users.ignored_post_behavior'] ||
+                app.forum.attribute('fof-ignore-users.ignored_post_default_behavior') ||
+                'hide',
+              onchange: (value) => {
+                this.user.savePreferences({ 'fof-ignore-users.ignored_post_behavior': value }).then(() => m.redraw());
+              },
+            })}
+
+            <p className="helpText">{app.translator.trans('fof-ignore-users.forum.settings.ignored_post_default_behavior_help')}</p>
+          </div>,
+        ]
+      ),
+      -11
     );
   });
-}
+};
